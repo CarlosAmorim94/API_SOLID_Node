87581f210016a432e1174a0321d2153e97cfbda6
carlos <carlosa@xgrow.com>
2023-10-20 22:25:49 -0300

    Test: Fixed return of use-case register

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,17 +1,26 @@
 import { UsersRepository } from "@/repositories/users-repository"
+import { User } from "@prisma/client"
 import { hash } from "bcryptjs"
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error"
 
-interface RegisterUseCaseProps {
+interface RegisterUseCaseRequest {
   name: string
   email: string
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: RegisterUseCaseProps) {
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     // Faz um hash da senha, o número 6 é o número de rounds que o bcrypt vai usar para gerar o hash, hash em cima de hash 6x, quanto maior o número mais seguro, mas mais lento.
     const password_hash = await hash(password, 6)
 
@@ -22,10 +31,14 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return {
+      user,
+    }
   }
 }
